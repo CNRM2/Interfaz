28f26b4d9d7a39bb405f4c07c3dea925c546d445
CNRM2 <l20600067@huatabampo.tecnm.mx>
2024-06-12 16:38:07 -0700

    Integracion del Captcha

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,18 +1,41 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom'; // Importa Link desde react-router-dom
+import { Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [captcha, setCaptcha] = useState('');
+    const [inputCaptcha, setInputCaptcha] = useState('');
+    const [showCaptchaModal, setShowCaptchaModal] = useState(false);
+
+    const generateCaptcha = () => {
+        const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        let captcha = '';
+        for (let i = 0; i < 6; i++) {
+            captcha += chars[Math.floor(Math.random() * chars.length)];
+        }
+        return captcha;
+    };
 
     const handleLogin = (e) => {
         e.preventDefault();
         if (username === 'admin' && password === 'password') {
-            setIsLoggedIn(true);
+            setCaptcha(generateCaptcha());
+            setShowCaptchaModal(true);
         } else {
             alert('Credenciales incorrectas');
+        }
+    };
+
+    const handleCaptchaSubmit = (e) => {
+        e.preventDefault();
+        if (inputCaptcha === captcha) {
+            setIsLoggedIn(true);
+            setShowCaptchaModal(false);
+        } else {
+            alert('CAPTCHA incorrecto');
         }
     };
 
@@ -20,11 +43,12 @@ function App() {
         <>
             {!isLoggedIn ? (
                 <div id='App' className="login-container">
+                    <h1>Inicio de Sesion</h1>
+                    <h2>Bienvenido a </h2>
                     <img src="src/assets/GasLogo.png" alt="Login Image" />
-                    <h1>Login</h1>
                     <form onSubmit={handleLogin}>
                         <div>
-                            <label htmlFor="username">User: </label>
+                            <label htmlFor="username">Usuario: </label>
                             <input
                                 type="text"
                                 id="username"
@@ -34,7 +58,7 @@ function App() {
                             />
                         </div>
                         <div>
-                            <label htmlFor="password">Password: </label>
+                            <label htmlFor="password">Contraseña: </label>
                             <input
                                 type="password"
                                 id="password"
@@ -43,10 +67,27 @@ function App() {
                                 required
                             />
                         </div>
-                        <button type="submit">Login</button>
+                        <button type="submit">Iniciar Sesion</button>
                     </form>
-                    {/* Usar Link para redirigir a la página de registro */}
                     <Link to="/registro/1">Registrarse</Link>
+
+                    {showCaptchaModal && (
+                        <div className="captcha-modal">
+                            <div className="captcha-content">
+                                <h2>Ingrese el texto <br/>para completar el Captcha</h2>
+                                <p>{captcha}</p>
+                                <form onSubmit={handleCaptchaSubmit}>
+                                    <input
+                                        type="text"
+                                        value={inputCaptcha}
+                                        onChange={(e) => setInputCaptcha(e.target.value)}
+                                        required
+                                    />
+                                    <button type="submit">No soy un Robot</button>
+                                </form>
+                            </div>
+                        </div>
+                    )}
                 </div>
             ) : (
                 <div>
